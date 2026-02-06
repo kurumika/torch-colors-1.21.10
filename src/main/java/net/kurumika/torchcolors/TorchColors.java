c34366a6557adb791bf696b3a9e04f6b02b86768
Kurumika <quinnawadeid@gmail.com>
2026-02-05 17:53:54 -0800

    Cloned the default torch

--- src/main/java/net/kurumika/torchcolors/TorchColors.java
+++ src/main/java/net/kurumika/torchcolors/TorchColors.java
@@ -2,23 +2,20 @@ package net.kurumika.torchcolors;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.kurumika.torchcolors.block.ModBlocks;
+import net.kurumika.torchcolors.item.ModItemGroups;
+import net.kurumika.torchcolors.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TorchColors implements ModInitializer {
 	public static final String MOD_ID = "torchcolors";
-
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		ModItemGroups.registerItemGroups();
+		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
---
+++ src/main/java/net/kurumika/torchcolors/item/ModItemGroups.java
@@ -0,0 +1,20 @@
+package net.kurumika.torchcolors.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.kurumika.torchcolors.TorchColors;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+public class ModItemGroups {
+    public static final ItemGroup TORCH_ITEM_GROUP = Registry.register(Registries.ITEM_GROUP, Identifier.of(TorchColors.MOD_ID, "torch_items"), FabricItemGroup.builder()
+            .icon(() -> new ItemStack(ModItems.CUSTOM_TORCH_ITEM)).displayName(Text.translatable("itemgroup.torchcolors.torch_items")).entries((displayContext, entries) -> {
+                entries.add(ModItems.CUSTOM_TORCH_ITEM);
+            }).build());
+    public static void registerItemGroups() {
+        TorchColors.LOGGER.info("Registering Item Groups for " + TorchColors.MOD_ID);
+    }
+}
